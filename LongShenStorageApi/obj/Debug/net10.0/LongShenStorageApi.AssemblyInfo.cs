852b979e36673379415119dfc336581731f07b22
jhf <jhf646@outlook.com>
2026-07-13 09:14:33 +0800

    更新exe

--- LongShenStorageApi/obj/Debug/net10.0/LongShenStorageApi.AssemblyInfo.cs
+++ LongShenStorageApi/obj/Debug/net10.0/LongShenStorageApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LongShenStorageApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f38265bf358272345a8e04f32b1aaf30806e2ad5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71f26f2dbe40bacdd9ecd79ef35e24697aefe48c")]
 [assembly: System.Reflection.AssemblyProductAttribute("LongShenStorageApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LongShenStorageApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
